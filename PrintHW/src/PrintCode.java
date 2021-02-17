79956497c071f78f287ffa30da29310f3b38ce81
Dragon <Dragon@DESKTOP-PL8UFPO>
2021-02-17 18:40:41 +0300

    first commit

---
+++ PrintHW/src/PrintCode.java
@@ -0,0 +1,15 @@
+
+public class PrintCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("My Name IS Abdullah Alyami");
+		
+		
+		
+		
+	}
+
+}
